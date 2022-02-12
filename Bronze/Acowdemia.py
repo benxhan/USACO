61e3eae0b26ac1f637fcfac3d8fd97dd8443e7ad
benxhan <98715186+benxhan@users.noreply.github.com>
2022-02-11 22:38:46 -0800

    Update Acowdemia.py

--- Bronze/Acowdemia.py
+++ Bronze/Acowdemia.py
@@ -2,16 +2,16 @@ lines = "4 0\n 1 100 2 3"
 lines = [int(i) for i in lines.strip().split(" ")]
 
 n = lines[0] # number of papers
-L = lines[1] # citations 
+L = lines[1] # citations
 
 true_h_index = 0
 for x in range(n):
     h_index = x+1
     citations = 0
     for y in range(n):
-        if lines[y+2] > h_index:
+        if lines[y+2] >= h_index:
             citations += 1
-    if (citations == h_index):
+    if (citations >= h_index):
         true_h_index = h_index
 
-print(true_h_index + L)
+print(true_h_index)
